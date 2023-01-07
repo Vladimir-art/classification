fa30b4bbd4650f4fca94897b10f2b620d921f8d3
Vladimir <vladimir.ermolaev@orioninc.com>
2023-01-07 18:59:35 +0000

    added main section 2

--- client/src/components/DisplayResultBox/index.tsx
+++ client/src/components/DisplayResultBox/index.tsx
@@ -1,7 +1,45 @@
+import { Box, Typography } from "@mui/material";
 import { FC } from "react";
+import ImageBox from "../commonComponents/ImageBox";
+import testPic from "../../image/test.jpg";
 
 const DisplayResultBox: FC = () => {
-  return <div>2</div>;
+  return (
+    <Box
+      component="div"
+      sx={{
+        display: "flex",
+        width: "50%",
+        flexDirection: "column",
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
+      {/* <Box component="div" sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "300px",
+        height: "300px",
+        borderRadius: "1rem"
+    }}>
+
+    </Box> */}
+      <ImageBox
+        path={testPic}
+        height={300}
+        width={300}
+        props={{
+          marginBottom: "1rem",
+          borderRadius: "1rem",
+          boxShadow: "15px -15px rgba(255, 239, 213, .3)",
+        }}
+      />
+      <Typography variant="h3" align="left" fontSize="1rem" fontWeight={500}>
+        Choosen picture is <Box component="span" sx={{ color: 'primary.main' }}>{"Flower"}</Box>
+      </Typography>
+    </Box>
+  );
 };
 
 export default DisplayResultBox;
