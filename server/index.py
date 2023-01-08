eb573607728ef8604adbcef47d39bc0627c2582c
Vladimir <vladimir.ermolaev@orioninc.com>
2023-01-08 13:14:19 +0000

    added server and fixed FE

--- server/index.py
+++ server/index.py
@@ -1,1 +1,12 @@
-print("Hello")
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['POST'], )
+def hello():
+    body = request.json
+    print(body['message'])
+    return 'Hello, world!'
+
