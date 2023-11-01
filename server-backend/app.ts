4113330c69d964fb2ea4f12b8e2c8d12f44fe5bc
Vladimir Ermolaev <vermolaev@natera.com>
2023-11-01 23:17:25 +0300

    added register and login routers

--- server-backend/app.ts
+++ server-backend/app.ts
@@ -1,6 +1,9 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import connect from "./config/database";
+import User from "./model/user";
 
 //For env File
 dotenv.config();
@@ -10,11 +13,70 @@ connect();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!(email && password && name)) {
+      res.status(400).send("All input is required");
+    }
+
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    const encryptedPassword = await bcryptjs.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY as string,
+      {
+        expiresIn: "2h",
+      }
+    );
+    user.token = token;
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.use(express.json());
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcryptjs.compare(password, user.password!))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY as string,
+        {
+          expiresIn: "2h",
+        }
+      );
+      user.token = token;
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
