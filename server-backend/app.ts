49c3dbeda34a4487fbb68c2cb6d1da44bec1d911
Vladimir Ermolaev <vermolaev@natera.com>
2023-11-02 22:28:48 +0300

    added github endpoints

--- server-backend/app.ts
+++ server-backend/app.ts
@@ -98,18 +98,14 @@ app.post("/classification", verifyToken, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
-app.get("/auth/google", passport.authenticate("google"), function (req, res) {
-  // The request will be redirected to Google for authentication, so
-  // this function will not be called.
-});
+app.get('/auth/github', passport.authenticate('github'));
 
 app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/classification");
-  }
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/classification',
+    failureRedirect: '/',
+  })
 );
 
 app.listen(port, () => {
